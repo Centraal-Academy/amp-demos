c39b79750e9dc36a58ebd3edcaf151fdef02b2fa
DezkaReid <elmaildeldezkareid@gmail.com>
2018-04-12 18:57:58 -0500

    react setup

---
+++ react-amp/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+const appContainer = document.getElementById('app-container')
+ReactDOM.render(<App />, appContainer)
